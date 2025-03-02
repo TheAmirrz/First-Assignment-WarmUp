c7e8f08b11fd3153aa6293977bf007a0ba9ceee9
Amir Gholami <amirrzh123@gmail.com>
2025-03-02 18:33:06 +0330

    First assignment - Warm up - Amir Gholami

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -1,48 +1,92 @@
 public class Exercises {
 
-    /*
-        complete this function to check if the input number is prime or not
-     */
     public boolean isPrime(long n) {
-        // todo
-        return false;
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    /*
-        implement an algorithm to find out the index of input number in a fibonacci sequence starting from 0, 1
-        e.g. 0, 1, 1, 2, 3, 5, ...
-        indices start from 0, e.g. 3 is the index 4 of this sequence
-        if the input is not a fibonacci number with description above, return -1
-     */
     public long fibonacciIndex(long n) {
-        // todo
+        if (n == 0) {
+            return 0;
+        }
+
+        // a holds the previous Fibonacci number, b holds the current.
+        long a = 0;
+        long b = 1;
+        long index = 1;
+
+        while (b <= n) {
+            if (b == n) {
+                return index;
+            }
+            long next = a + b;
+            a = b;
+            b = next;
+            index++;
+        }
         return -1;
     }
 
-    /*
-        you should create a triangle with "*" and return a two-dimensional array of characters based on that
-        the triangle's area is empty, which means some characters should be " "
 
-        example 1, input = 3:
-        *
-        **
-        ***
-
-        example 2, input = 5:
-        *
-        **
-        * *
-        *  *
-        *****
-
-        the output has to be a two-dimensional array of characters, so don't just print the triangle!
-     */
     public char[][] generateTriangle(int n) {
-        // todo
-        return null;
+        char[][] triangle = new char[n][];
+
+        for (int i = 0; i < n; i++) {
+            // Create a new row with a length equal to the row number + 1.
+            triangle[i] = new char[i + 1];
+
+            // For the first two rows and the last row
+            if (i < 2 || i == n - 1) {
+                for (int j = 0; j <= i; j++) {
+                    triangle[i][j] = '*';
+                }
+            }
+            else {
+                // For the middle rows:
+                triangle[i][0] = '*';
+                triangle[i][i] = '*';
+
+                for (int j = 1; j < i; j++) {
+                    triangle[i][j] = ' ';
+                }
+            }
+        }
+
+        return triangle;
     }
 
     public static void main(String[] args) {
-        // you can test your code here, but then it should be checked with test cases
+        Exercises ex = new Exercises();
+
+        // Testing triangle generation function
+        int triangleSize = 5;
+        char[][] triangle = ex.generateTriangle(triangleSize);
+        System.out.println("Generated triangle:");
+        for (char[] row : triangle) {
+            System.out.println(new String(row));
+        }
+
+        // Testing fibo function
+        long fibNumber = 987;
+        long index = ex.fibonacciIndex(fibNumber);
+        System.out.println("Fibonacci index of " + fibNumber + " is: " + index);
+
+        // Testing isPrime function
+        long candidate = 17;
+        boolean isPrime = ex.isPrime(candidate);
+        System.out.println(candidate + " is prime? " + isPrime);
     }
 }
